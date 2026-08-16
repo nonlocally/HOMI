eef3cbeceee1247b8ee145525366874b75124adb
Aadarsh Agarwal <aadarsh@uchicago.edu>
2026-08-16 09:13:31 -0400

    docs: v2 README (verbs + 161-check verification + live proofs), overnight handoff, research studies

--- lib/homi.sh
+++ lib/homi.sh
@@ -33,7 +33,7 @@ homi_cmd() {
     uninstall) homi_uninstall "$@";;
     adopt)     homi_adopt "$@";;
     retitle)   python3 "$HOMI_PY" retitle "$@";;
-    *) die "usage: communicate homi {start|stop|status [--json]|claim|release|send|inbox|link|unlink|install|uninstall|adopt|retitle} ...";;
+    *) die "usage: communicate homi {start|stop|status|agents|claim [--boxed]|release|send|ask|reply|wait|group|notify|inbox|seat|spawn|fan|consult|move|link|unlink|grant|ungrant|grants|card|federate|install|uninstall|adopt|retitle} ...";;
   esac
 }
 
